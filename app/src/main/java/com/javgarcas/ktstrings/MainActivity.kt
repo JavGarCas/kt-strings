ce6d2fb27620a86f845a2f1df75fe0ca7a08363d
JavGarCas <javgacas@gmail.com>
2024-04-21 11:39:17 -0600

    Test library

--- app/src/main/java/com/javgarcas/ktstrings/MainActivity.kt
+++ app/src/main/java/com/javgarcas/ktstrings/MainActivity.kt
@@ -3,6 +3,9 @@ package com.javgarcas.ktstrings
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -13,16 +16,34 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.javgarcas.ktstrings.ui.theme.KtStringsTheme
 
 class MainActivity : ComponentActivity() {
+    private val emailValues = listOf("user@domain.com", "wrongemail@domain", "user@domain.xx")
+    private val phoneValues = listOf("123","123123123","1231231230")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             KtStringsTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    Column {
+                        Greeting("Android")
+                        emailValues.forEach {
+                            RegexTest(value = it, result = it.isValidEmail().toString())
+                        }
+                        phoneValues.forEach {
+                            RegexTest(value = it, result = it.isValidPhone().toString())
+                        }
+                    }
                 }
             }
         }
+    }
+}
+
+@Composable
+fun RegexTest(value: String, result: String) {
+    Row(horizontalArrangement = Arrangement.SpaceBetween) {
+        Text(text = value)
+        Text(text = result)
     }
 }
 
