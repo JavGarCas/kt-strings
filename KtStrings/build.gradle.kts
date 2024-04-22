fdede85e648d6b95f05b5b66024ab806c425b932
JavGarCas <javgacas@gmail.com>
2024-04-21 18:15:50 -0600

    Publish to github packages

--- KtStrings/build.gradle.kts
+++ KtStrings/build.gradle.kts
@@ -1,6 +1,14 @@
+import java.io.FileInputStream
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    id("maven-publish")
+}
+
+val githubProperties = Properties().apply {
+    load(FileInputStream(rootProject.file("github.properties"))) // Load properties from file
 }
 
 android {
@@ -8,7 +16,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        minSdk = 24
+        minSdk = 8
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
@@ -29,6 +37,33 @@ android {
     }
     kotlinOptions {
         jvmTarget = "1.8"
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("gpr") {
+            run {
+                groupId = "com.github.javgarcas"
+                artifactId = "ktstrings"
+                version = "0.1.1"
+                artifact("${project.layout.buildDirectory.get().asFile}/outputs/aar/KtStrings-release.aar")
+            }
+        }
+    }
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            /** Configure path of your package repository on Github
+             *  Replace GITHUB_USERID with your/organisation Github userID and REPOSITORY with the repository name on GitHub
+             */
+            url = uri("https://maven.pkg.github.com/JavGarCas/kt-strings") // Github Package
+            credentials {
+                //Fetch these details from the properties file or from Environment variables
+                username = githubProperties["gpr.usr"] as String? ?: System.getenv("GPR_USER")
+                password = githubProperties["gpr.key"] as String? ?: System.getenv("GPR_API_KEY")
+            }
+        }
     }
 }
 
